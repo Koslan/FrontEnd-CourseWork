0b7e06a0b683d85a15823b4d3051a703eee77c97
Elmimira <elmira.volohova@gmail.com>
2023-10-14 13:35:30 +0300

    Add header and sidebar

---
+++ LinguaFlix/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import MovieDetails from './MovieDetails';
+import "./App.css"
+
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <div className="main-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/movie/:id" element={<MovieDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
