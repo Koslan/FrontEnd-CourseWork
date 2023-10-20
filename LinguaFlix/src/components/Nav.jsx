b2b38d9aeebbd1fcacbc2f2a1ccb8601939bc4bc
Kostiantyn Buriak <koslan843@gmail.com>
2023-10-20 18:04:40 +0300

    add addMovies

--- LinguaFlix/src/components/Nav.jsx
+++ LinguaFlix/src/components/Nav.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
-import '../App.css'; 
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 function Nav() {
-    return (
-        <nav>
-                <Link to="/movies">Movies</Link>
-                <Link to="/about_team">About team</Link>
-                <Link to="/about_project">About Project</Link>
-        </nav>
-    );
+  return (
+    <nav>
+      <Link to="/movies">Movies</Link>
+      <Link to="/add_movie">Add Movie</Link>
+      <Link to="/about_team">About team</Link>
+      <Link to="/about_project">About Project</Link>
+    </nav>
+  );
 }
 
 export default Nav;
